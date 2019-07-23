2503b119b2b017162d5fc42e14a48dd86cb128e8
“ujjwalmittal291” <ujjwalmittal@live.com>
2019-07-23 18:21:55 +0530

    Update

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,18 +16,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  submitted = false;
+  signupForm1: FormGroup;
+  formBuilder: FormBuilder
+  mobilenumber = new FormControl('');
+  public sendOTPobj: any = {
+    "mdn": "string",
+    "otp": 0
+  };
+  constructor(private http: HttpClient, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
-    let num:any;
+    this.signupForm1 = new FormGroup({
+      mobilenumber: this.mobilenumber
+    });
   }
-  check:(num:string)=>void = function(num:string):void {
-    // if (isNumber(num)) {
-      console.log(num);
-      // return true;
-    // }
+  // check:(num:string)=>void = function(num:string):void {
+  // if (isNumber(num)) {
+  // console.log(num);
+  // return true;
+  // }
+  onSubmit() {
+    this.submitted = true;
+    if (this.signupForm1.invalid) {
+      return;
+    }
+    console.log("onsubmit....", this.getPwdControls.mobilenumber.value);
+    this.router.navigate(['/inputotp']);
+    const mobileObj: any = {
+      mdn: this.mobilenumber.value,
+      otp: "1234"
+    }
+    this.http.post('https://test.dikshatech.com/windmill/user-services/v1/wm/guest/otp/send', mobileObj).subscribe((success) => {
+      console.log("success...", success);
+    })
   }
-  
 
+  get getPwdControls() {
+    return this.signupForm1.controls;
+  }
 }
